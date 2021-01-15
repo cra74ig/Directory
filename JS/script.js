210b209ed3f7ce95272f78093ff692996e39c9bf
cra74ig <ctbutton@sky.com>
2021-01-15 10:35:38 +0000

    Drop down menus now working as intended 15/01/2021

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,51 @@
-$("#add").hover(function(){
-    $(this).css("background-color", "green");
-    $("#addDropDown").css("visibility","visible")
-    }, function(){
-        setTimeout( () => {
-    $(this).css("background-color", "#333");
-    $("#addDropDown").css("visibility","hidden")
-        },3000)
-  });
+$("#add").click(function(){
+    if ($("#editDropDown").hasClass("active")){
+        $("#editDropDown").removeClass("active");
+        $("#edit").css("background-color", "#333");
+    }
+    if ($("#deleteDropDown").hasClass("active")){
+        $("#deleteDropDown").removeClass("active");
+        $("#delete").css("background-color", "#333");
+    }
+    if ($("#addDropDown").hasClass("active")){
+        $("#addDropDown").removeClass("active");
+        $(this).css("background-color", "#333");
+    }else{
+        $("#addDropDown").addClass("active");
+        $(this).css("background-color", "green");
+    }
+});
+$("#edit").click(function(){
+    if ($("#addDropDown").hasClass("active")){
+        $("#addDropDown").removeClass("active");
+        $("#add").css("background-color", "#333");
+    }
+    if ($("#deleteDropDown").hasClass("active")){
+        $("#deleteDropDown").removeClass("active");
+        $("#delete").css("background-color", "#333");
+    }
+    if ($("#editDropDown").hasClass("active")){
+        $("#editDropDown").removeClass("active");
+        $(this).css("background-color", "#333");
+    }else{
+        $("#editDropDown").addClass("active");
+        $(this).css("background-color", "green");
+    }
+});
+$("#delete").click(function(){
+    if ($("#addDropDown").hasClass("active")){
+        $("#addDropDown").removeClass("active");
+        $("#add").css("background-color", "#333");
+    }
+    if ($("#editDropDown").hasClass("active")){
+        $("#editDropDown").removeClass("active");
+        $("#edit").css("background-color", "#333");
+    }
+    if ($("#deleteDropDown").hasClass("active")){
+        $("#deleteDropDown").removeClass("active");
+        $(this).css("background-color", "#333");
+    }else{
+        $("#deleteDropDown").addClass("active");
+        $(this).css("background-color", "green");
+    }
+});
