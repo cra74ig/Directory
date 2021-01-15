5bd8ec31a841c1d1ec764379f42ce1f1a2223d9e
cra74ig <ctbutton@sky.com>
2021-01-15 11:56:47 +0000

    modal inputs for addContact created 15/01/2021

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,17 @@
 $("#add").click(function(){
+    $(this).prop("selectedIndex", 0);
     if ($("#editDropDown").hasClass("active")){
         $("#editDropDown").removeClass("active");
         $("#edit").css("background-color", "#333");
-        $("#editDropDown").prop("selectedIndex", 0);
+        
     }
     if ($("#deleteDropDown").hasClass("active")){
         $("#deleteDropDown").removeClass("active");
         $("#delete").css("background-color", "#333");
-        $("#addDropDown").prop("selectedIndex", 0);
+        
     }
     if ($("#addDropDown").hasClass("active")){
-        $(this).prop("selectedIndex", 0);
+        
         $("#addDropDown").removeClass("active");
         $(this).css("background-color", "#333");
         
@@ -20,40 +21,36 @@ $("#add").click(function(){
     }
 });
 $("#edit").click(function(){
+    $(this).prop("selectedIndex", 0);
     if ($("#addDropDown").hasClass("active")){
         $("#addDropDown").removeClass("active");
         $("#add").css("background-color", "#333");
-        $("#addDropDown").prop("selectedIndex", 0);
     }
     if ($("#deleteDropDown").hasClass("active")){
         $("#deleteDropDown").removeClass("active");
-        $("#delete").css("background-color", "#333");
-        $("#deleteDropDown").prop("selectedIndex", 0);
+        $("#delete").css("background-color", "#333");     
     }
     if ($("#editDropDown").hasClass("active")){
         $("#editDropDown").removeClass("active");
-        $(this).css("background-color", "#333");
-        $(this).prop("selectedIndex", 0);
+        $(this).css("background-color", "#333");  
     }else{
         $("#editDropDown").addClass("active");
         $(this).css("background-color", "green");
     }
 });
 $("#delete").click(function(){
+    $(this).prop("selectedIndex", 0);
     if ($("#addDropDown").hasClass("active")){
         $("#addDropDown").removeClass("active");
         $("#add").css("background-color", "#333");
-        $("#addDropDown").prop("selectedIndex", 0);
     }
     if ($("#editDropDown").hasClass("active")){
         $("#editDropDown").removeClass("active");
         $("#edit").css("background-color", "#333");
-        $("#editDropDown").prop("selectedIndex", 0);
     }
     if ($("#deleteDropDown").hasClass("active")){
         $("#deleteDropDown").removeClass("active");
         $(this).css("background-color", "#333");
-        $(this).prop("selectedIndex", 0);
     }else{
         $("#deleteDropDown").addClass("active");
         $(this).css("background-color", "green");
